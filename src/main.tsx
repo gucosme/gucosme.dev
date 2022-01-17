102dcb6a1440cba5765886edf969c20b1d9228bb
Gustavo Cosme <gcosme@twilio.com>
2022-01-17 17:59:25 -0300

    feat: use installed fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import { createTheme, ThemeProvider } from "@mui/material";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
